84b7c4d3554ea7cbf3ee85dd2ce6b1a4b468f6a4
Ejaz Ali <ejaz84916@gmail.com>
2023-09-10 18:37:24 +0530

    Update UserDashboard.jsx

--- components/dashboard/UserDashboard.jsx
+++ components/dashboard/UserDashboard.jsx
@@ -1,17 +1,11 @@
-"use client"
-
 import React from 'react'
 
 const UserDashboard = () => {
   return (
-    <div className='h-[70vh] md:w-3/4 m-auto flex justify-center items-center'>
-    <div className='md:w-2/6 py-10 m-auto flex flex-col justify-center items-center border bg-orange-300 shadow-xl'>
-     <h1 className='text-center py-4'> user data</h1>
-     <div className='w-full lg:w-[300px] flex justify-center items-center p-2'>
+    <div className='h-[100vh] md:w-10/12 m-auto flex '>
+     <div className='w-full text-center  p-2 bg-white'>
       Our Dashboard
      </div>
-
-    </div>
     </div>
   )
 }
